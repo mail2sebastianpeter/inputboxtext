57968ed7d21fa031f38334c2bc3e4e3f320d44d9
= <=>
2020-04-28 16:56:16 +0530

    Added 3 test cases to the TestMessageService Page and modified the MessageService page

---
+++ src/main/java/com/MessageService.java
@@ -0,0 +1,23 @@
+package com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageService {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello Engineers";
+	}
+	
+	@GetMapping("/devops")
+	public String welcomeDevops(){
+		return "Welcome to the world of DevOps";
+	}
+	
+	@GetMapping("/page1")
+	public String Page1(){
+		return "This is a Sample text For testing the pages";
+	}
+}
---
+++ src/test/java/com/TestMessageService.java
@@ -0,0 +1,34 @@
+package com;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestMessageService {
+	
+	@Test
+	public void testMessage() {
+		MessageService MessageService = new MessageService();
+		System.out.println("hello");
+		Assertions.assertEquals(MessageService.sayHello(), "Hello Engineers");
+	}
+	
+	@Test
+	public void testdevops() {
+		MessageService ms = new MessageService();
+		System.out.println("Successfully validated the devops test csse");
+		Assertions.assertEquals(ms.welcomeDevops(), "Welcome to the world of DevOps");
+	}
+	
+	@Test
+	public void Page1() {
+		MessageService ms = new MessageService();
+		System.out.println("This is a Sample text For testing the pages");
+		Assertions.assertEquals(ms.Page1(), "This is a Sample text For testing the pages");
+	}
+}
